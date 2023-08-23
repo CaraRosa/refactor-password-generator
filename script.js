7e0c5b1e22ef2b9325fba385ec48517c1fd7f433
Cara R <cara.rosa.02@gmail.com>
2023-08-22 23:08:44 -0500

    added new password generator code

--- script.js
+++ script.js
@@ -19,8 +19,8 @@ function generatePassword() {
 
 // While Loop to check if password's set parameters are met
 while (confirmLength <= 8 || confirmLength >= 128) {
-  alert("The required length for the password is between 8-128 characters. Please start over.");
   var confirmLength = (prompt("Please enter the number of characters your password will have: ")); 
+  alert("The required length for the password is between 8-128 characters. Please start over.");
   
 }
 // Alerts the user to the character length of the password
@@ -45,14 +45,18 @@ var confirmSpecialChar = confirm("Please click OK to confirm that your password 
   var passwordCharacters = [];
 
 // if loops adding on characters to the end of the password
+
 if(confirmLowerCase) {
+
   passwordCharacters = passwordCharacters.concat(lowercaseLetters)
 }
 
 if(confirmUpperCase) {
   passwordCharacters = passwordCharacters.concat(uppercaseLetters)
 }
-
+// debugger;
+console.log("Confirm Numberic Character", confirmNumericChar);
+console.log(numbers);
 if(confirmNumericChar) {
   passwordCharacters = passwordCharacters.concat(numbers)
 }
@@ -62,24 +66,101 @@ if(confirmSpecialChar) {
 }
 
 // Logs the password characters
-  console.log(passwordCharacters);
+  console.log("Password Character", passwordCharacters);
 
-var yourPassword = " ";
+var yourPassword = "";
+// password = ''
 
-for (var i = 0; i < confirmLength; i++) {
-  yourPassword = yourPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
-  console.log(yourPassword);
+
+  // Generate random number for each req iteration.
+  // 9
+   // Random generate a number for each passord req.
+   
+  // 
+
+  // Array for upper case
+  //[...]
+
+  // Array for lowercase case
+  //[...]
+
+  // Array for special charctger case
+  //[...]
+
+  // Array for number case
+  //[...]
+  // Password length is 8
+  // Requirement is 4 or all of them
+  // Take password length and divide it by requirements (8/4)
+  // Loop through each requirement array x times 
+  // Take all values and put them together into an array
+  // *Randomize order in the array
   
-}
-  return yourPassword;
+  // If password length is 9:
+  //Requirement is 4 or all of them
+  //Take password length and divide it by requirements (9/4)
+
+  function getRequirementCount(length, requirementCount) {
+    // length is 9
+    // only one requirement: lowercase characters
+    // return 9
+
+    // if length is 9 and requirement is all 4
+    // Take length and subtract it by the requirement plus 1
+  }
+
+  var myPassword = "";
+  // Loop through lowercase characters
+  // var confirmLowerCase = confirm("Please click OK to confirm that your password will contain lowercase letters.");
+  // new password genrator
+
+  for (var i = 0; i < confirmLength; i++) {
+    myPassword = myPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+    // all characters
+    // passwordCharacters
+    // random number
+    // Math.floor(Math.random() * passwordCharacters.length)
+    
+    if (confirmLowerCase) {
+      myPassword = myPassword + lowercaseLetters[Math.floor(Math.random() * lowercaseLetters.length)];
+    }
+    console.log(yourPassword);
+
+    if (confirmUpperCase) {
+      myPassword = myPassword + uppercaseLetters[Math.floor(Math.random() * uppercaseLetters.length)];
+    }
+
+    if (confirmNumericChar) {
+      myPassword = myPassword + numbers[Math.floor(Math.random() * numbers.length)];
+    }
+
+    if (confirmSpecialChar) {
+      yourPassword = yourPassword + specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+    }
+  
+  }
+  console.log("My password version 2", yourPassword);
+
+  // my initial password generation
+// for (var i = 0; i < confirmLength; i++) {
+//   yourPassword = yourPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+//   // all characters
+//   // passwordCharacters
+//   // random number
+//   // Math.floor(Math.random() * passwordCharacters.length)
+
+//   console.log(yourPassword);
+  
+// }
+  return myPassword;
 }
 
 
 function writePassword() {
   var password = yourPassword();
-  var passwordTex = document.querySelector("#password");
+  var passwordText = document.querySelector("#password");
 
-  passwordTex.value = password;
+  passwordText.value = password;
 }
 
 // Get references to the #generate element
