41498680ba8a79f9d320f43f59ab2c6610710e27
Cara R <cara.rosa.02@gmail.com>
2023-08-24 10:49:04 -0500

    deleted unnecessary query selector and add event listener

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Assignment code here
-document.querySelector("#generate").addEventListener("click", writePassword);
 
 // Declare arrays
 var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
